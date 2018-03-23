d40c55955e70401b3bb4d36222cf8e25549d35c0
cdplourde <cdplourde@gmail.com>
2018-03-23 15:13:44 -0700

    added search functionality displaying 10 results

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,8 +4,12 @@ function appendRow(selector, id) {
 }
 
 //append new column to HTML. Takes in a selector, what gets inserted in the div, column specifics (EX. "-md-6"), and an ID name
-function appendCol(selector, id, col) {
-    $(selector).append("<div class='col" + col + "' id='" + id + "'></div>");
+function appendCol(selector, id, colClass, col) {
+    $(selector).append("<div class='" + colClass + " col" + col + "' id='" + id + "'></div>");
+}
+
+function createResults() {
+
 }
 
 function googleQuery(title) {
@@ -18,27 +22,60 @@ function googleQuery(title) {
     //     createRow();
     //     createCol();
     // });
-    $.get(queryURL).then(function(response) {
-        var books = response.items;
-        console.log(books);
-        //create new row and column for results
-        appendRow(".container");
-        appendCol(".row:last-of-type", "results-col");
-        $("#results-col").append("<ul id='results'></ul>")
-        for (var i = 0; i < books.length; i++) {
-            var book = books[i].volumeInfo;
-            var thumbnail = book.imageLinks.smallThumnail;
-            var reviews = book.averageRating;
-            var authors = book.authors; //LOOP THROUGH
-            var pageCount = book.pageCount;
-            var publishDate = book.publishedDate;
-            var title = book.title;
-            $("#results").append("<li id='book" + i + "'></li>");
-            $("#book" + i).html("<h3 id='book" + i + "title'>" + title + "</h3>");
+    $.get(queryURL)
+    .done(function(response) {
+        console.log(response);
+        //only run code if there are more than 0 results
+        $("#results-row1").remove();
+        $("#results-row2").remove();
+        if (response.totalItems !== 0) {
+            var books = response.items;
+            //create new row and column for results
+            var numRows = 1;
+            var numCols = response.items.length;
+            for (var i = 1; i < numRows + 1; i++) {
+                appendRow(".container", "results-row" + i);
+                for (var j = 1; j < numCols + 1; j++) {
+                    appendCol("#results-row" + i, "results" + i + "-col" + j, "book-results-col", "-2");
+                }
+            }  
+            for (var j = 1; j < numCols + 1; j++) {
+                $("#results1-col" + j).append("<div id='book" + j + "'></div>");
+            } 
+            //get info for each book
+            for (var i = 0; i < books.length; i++) {
+                var book = books[i].volumeInfo;
+                //grab key names to verify certain keys exist
+                var objKeys = Object.keys(book);
+                console.log(book);
+                // if (objKeys.indexOf("imageLink") !== -1) {
+                    var thumbnail = book.imageLinks.smallThumbnail;
+                    console.log(thumbnail);
+                // };
+                var reviews = book.averageRating;
+                var author = book.authors[0];
+                var pageCount = book.pageCount;
+                var publishDate = book.publishedDate;
+                var title = book.title;
+                if (title.length < 40) {
+                    $("#book" + (i + 1)).html("<p>" + title + "</p><i class='fas fa-plus'></i><img src='" + thumbnail + "'></p>" + author);  
+                }      
+                else {
+                    title = title.slice(0, 40);
+                    title = title + "..."
+                    $("#book" + (i + 1)).html("<p>" + title + "</p><i class='fas fa-plus'></i><img src='" + thumbnail + "'></p>" + author);  
+                }       
+            }   
+        }
+        else {
+            //if no results are found tell the user
+            appendRow(".container", "results-row");
+            appendCol(".row:last-of-type", "results-col", "book-results-col");
+            $("#results-col").append("<h2>No results! :-(</h2>")
         }
     });
 }
-function semanticQuery() {
+function semanticQuery(title) {
 
 }
 
